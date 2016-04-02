733430a2cbd1eae1ce629e000a4e20dc7efc1f40
Paul Rene Nichols <paul.rene.nichols@gmail.com>
2016-04-02 16:58:48 -0500

    react-router
    * bumping version to 0.2.0
    * adding react-router, routing

--- client/store/configureStore.prod.js
+++ client/store/configureStore.prod.js
@@ -2,11 +2,21 @@ import { createStore, applyMiddleware, compose }  from 'redux';
 import { rootReducer }                            from '../reducers/index';
 import reduxStateAndLogThunkMiddleware            from '../middleware/reduxStateAndLogThunkMiddleware';
 import sequenceAction                             from 'redux-sequence-action';
+import { syncHistory }                            from '../router/react-router-redux-immutable';
+import { browserHistory }                         from 'react-router';
+
+const reduxRouterMiddleware = syncHistory(browserHistory);
 
 const enhancer = compose(
-  applyMiddleware(sequenceAction, reduxStateAndLogThunkMiddleware)
+  applyMiddleware(sequenceAction, reduxStateAndLogThunkMiddleware, reduxRouterMiddleware)
 );
 
 export default function configureStore(initialState) {
-  return createStore(rootReducer, initialState, enhancer);
+  const store = createStore(rootReducer, initialState, enhancer);
+
+  reduxRouterMiddleware.listenForReplays(store);
+
+  store.browserHistory = browserHistory;
+
+  return store;
 };
